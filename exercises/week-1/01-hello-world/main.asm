125c0dbd03f115c1ba803e0c91e49deff3d7c6f0
alansouza1 <alansouza4001@gmail.com>
2026-03-24 11:56:34 -0300

    feat: initial setup for x86_64 NASM environment with Docker

---
+++ exercises/week-1/01-hello-world/main.asm
@@ -0,0 +1,19 @@
+section .data
+    msg db "Hello, FIAP!", 0x0A ; 0x0A is newline
+    msg_len equ $ - msg
+
+section .text
+    global _start
+
+_start:
+    ; syscall: write(1, msg, msg_len)
+    mov rax, 1          ; syscall number for write
+    mov rdi, 1          ; file descriptor 1 (stdout)
+    mov rsi, msg        ; pointer to the message
+    mov rdx, msg_len    ; message length
+    syscall
+
+    ; syscall: exit(0)
+    mov rax, 60         ; syscall number for exit
+    xor rdi, rdi        ; exit code 0
+    syscall
