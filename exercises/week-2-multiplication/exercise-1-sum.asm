fea53352ad516b54a41b45e03417e6de86dbe831
alansouza1 <alansouza4001@gmail.com>
2026-03-30 14:17:00 -0300

    feat: Add exercise 1

---
+++ exercises/week-2-multiplication/exercise-1-sum.asm
@@ -0,0 +1,39 @@
+section .data
+
+num1 db 5
+num2 db 3
+
+msg db "Resultado: "
+tam equ $-msg
+
+section .bss
+
+res resb 1
+
+section .text
+
+global _start
+
+_start:
+
+    mov al, [num1]
+    add al, [num2]
+
+    add al, 48
+    mov [res], al
+
+    mov eax,4
+    mov ebx,1
+    mov ecx,msg
+    mov edx,tam
+    int 0x80
+
+    mov eax,4
+    mov ebx,1
+    mov ecx,res
+    mov edx,1
+    int 0x80
+
+    mov eax,1
+    int 0x80
+
